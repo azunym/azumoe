41726f5228f0a21aea9770caadef9d08dc9fed6d
azunymous <azu@azunymo.us>
2016-11-11 01:38:42 +0000

    Added random logo js and new logo

---
+++ js/logo.js
@@ -0,0 +1,9 @@
+var random_logos = ["banazu1_fight.gif", "azukyuu.gif"];
+
+function getRandomImage(imgAr, path) {
+    path = path || 'img/'; // default path here
+    var num = Math.floor( Math.random() * imgAr.length );
+    var img = imgAr[ num ];
+    var imgStr = '<img src="' + path + img + '" alt = "azu" class="boxed">';
+    document.write(imgStr); document.close();
+}
